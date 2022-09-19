27a75cce4dbfd2afea84a316cdb7c9065ac4efc7
Vismay Patel <vismaypikachu@gmail.com>
2022-09-19 22:26:50 +0000

    css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react"
+import "./components/Navbar.css"
 
 const Navbar = () =>{
     return <nav>
+        <a>
+            <img src="./public/logo192.png"/>
+        </a>
         <a class="active" href="/">Home</a>
         <a href="/">News</a>
         <a href="/">Contact</a>
