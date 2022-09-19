e64a2d60c608f91dffd5d0c1de23386a53f3fb4c
Vismay Patel <vismaypikachu@gmail.com>
2022-09-19 22:30:40 +0000

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css"
 const Navbar = () =>{
     return <nav>
         <a>
-            <img src="./public/logo192.png"/>
+            <img src="/public/logo192.png"/>
         </a>
         <a class="active" href="/">Home</a>
         <a href="/">News</a>
