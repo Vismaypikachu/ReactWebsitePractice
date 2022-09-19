dead058a087e926bcf752f0d4c02176264ce0948
Vismay Patel <vismaypikachu@gmail.com>
2022-09-19 21:04:48 +0000

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+Navbar = () =>{
+    <nav>
+        <a class="active" href="/">Home</a>
+        <a href="/">News</a>
+        <a href="/">Contact</a>
+        <a href="/">About</a>
+    </nav>
+}
+
+export default Navbar;
